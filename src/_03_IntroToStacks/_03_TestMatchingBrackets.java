a7d188310b063c6099a417d65fbb2ed8bea60f49
agudi24 <league@WTS-IM-17.attlocal.net>
2019-03-31 15:54:18 -0700

    TestMatchingBrackets (IP)

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -18,16 +18,34 @@ public class _03_TestMatchingBrackets {
 		assertFalse(doBracketsMatch("}{"));
 	}
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
+	// A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		Stack<String> brackets = new Stack<String>();
-		brackets.push(b);
-		if(brackets.contains("{}")) {
-		return true;
+		Stack<Character> brackets = new Stack<Character>();
+		for(int i = 0; i < b.length(); i++) {
+			brackets.push(b.charAt(i));
+		}
+		int matched = 0;
+		System.out.println(brackets.pop());
+		for(int i = 0; i <= brackets.size(); i++) {
+			char popped = brackets.pop();
+			System.out.println(popped);
+			if(popped == '}') {
+				matched++;
+			}
+			else if(popped == '{'){
+				matched--;
+			}
+		}
+		System.out.println(matched);
+		if(matched == 0) {
+			return true;
 		}
 		else {
-		return false;
+			return false;
 		}
 	}
-
 }
+
+
+
